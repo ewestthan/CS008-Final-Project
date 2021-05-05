961b0e6a41ce8811c7fdd4ab638d062f7595bd0b
wtaylor-collab <crabbyboy24@gmail.com>
2021-05-05 12:54:12 -0400

    content addition
    - added text to gallery page

--- gallery.php
+++ gallery.php
@@ -1,5 +1,7 @@
 <?php include 'top.php';?>
 <main>
+        <h2>Sample Works</h2>
+        <p>Below are four sample works from our current collection, comprising a wide range of artists, from the abstract works of Piet Mondrian to the realistic works of Claude Monet. Artem Galleries prides itself on this range, as it allows for all who visit to find a piece they enjoy observing.</p>
         <figure>
                 <img alt="haring" src="images/haring.jpeg">
                 <figcaption><cite><a href = "https://img.kingandmcgaw.com/imagecache/4/3/bmwcm-5.0_fid-880229_fwcm-1.6_ihcm-50.0_iwcm-50.0_lmwcm-5.0_maxdim-1000_mc-ffffff_rmwcm-5.0_si-435786.jpg_tmwcm-5.0.jpg" target = "_blank" >Untitled by Keith Haring</a></cite></figcaption>
@@ -7,7 +9,7 @@
        
         <figure>
                 <img alt="monet" src="images/monet.jpeg">
-                <figcaption><cite><a href = "https://images.saatchiart.com/saatchi/845756/art/2975166/additional_1f5cbd59172ccfef315ecff34f15c653df226999-8.jpg" target = "_blank" >Water >Lilies by Monet</a></cite></figcaption>
+                <figcaption><cite><a href = "https://images.saatchiart.com/saatchi/845756/art/2975166/additional_1f5cbd59172ccfef315ecff34f15c653df226999-8.jpg" target = "_blank" >Water Lilies by Monet</a></cite></figcaption>
         </figure>
         
         <figure>
@@ -19,6 +21,8 @@
                 <img alt="hopper" src="images/hopper.jpeg">
                 <figcaption><cite><a href = "https://images-na.ssl-images-amazon.com/images/I/71jRxhVlvjL._AC_SL1415_.jpg" target = "_blank" >Nighthawks by Edward Hopper</a></cite></figcaption>
         </figure>
+        <h2>Want to Buy?</h2>
+        <p>If you are interested in purchasing a replica or the original work of any piece shown here, please head on over to our sales form and fill it out thoroughly. The information you provide there will help us in figuring out your request, and allow us to come to a deal you are happy with!</p>
 </main>
 <?php include 'footer.php'; ?>
 </body>
